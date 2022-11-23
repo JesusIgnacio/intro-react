b43f25f19f5629490476ac6b653badb987a08d40
chunacho <ignacio.casbar@gmail.com>
2022-11-23 15:19:10 -0300

    without react

---
+++ without-react/index.js
@@ -0,0 +1,5 @@
+let divElem = document.getElementById("root");
+let header = document.createElement("header");
+var t = document.createTextNode("Learn React");
+header.appendChild(t);
+divElem.appendChild(header);
