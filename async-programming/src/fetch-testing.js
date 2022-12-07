cf1beb28af7bf84bcb8a5eb5348cc42c681ac97f
JCastillo <ignacio.casbar@gmail.com>
2022-12-07 19:42:59 +0000

    delete

--- async-programming/src/fetch-testing.js
+++ async-programming/src/fetch-testing.js
@@ -73,7 +73,31 @@ let sincronyzeTodos = async (todos, username) => {
     });
 }
 
+let deleteTodos = async (username) => {
+    await fetch('https://assets.breatheco.de/apis/fake/todos/user/'.concat(username), {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => {
+        console.log(resp.ok); // will be true if the response is successfull
+        console.log(resp.status); // the status code = 200 or code = 400 etc.
+        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+    })
+    .then(data => {
+        //here is were your code should start after the fetch finishes
+        console.log(data); //this will print on the console the exact object received from the server
+    })
+    .catch(error => {
+        //error handling
+        console.log(error);
+    });
+}
 
-await createUser(myUser)
-await sincronyzeTodos(myTodos, myUser)
+
+//await createUser(myUser)
+//await sincronyzeTodos([], myUser)
+//await getTodos(myUser)
+await deleteTodos(myUser)
 await getTodos(myUser)
